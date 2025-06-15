dde786fe66d268edbd0160cc82153d05657bae08
Alex De Luca <alexnaj88@gmail.com>
2025-06-15 15:49:02 -0400

    feat: setting gh-pages config/scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/odin-admin-dashboard-react",
   server: {
     open: true,
     port: 8080
